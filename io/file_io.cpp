bb0c85174e58e321fa3aca26bc7811d833eef430
ZackLackinJava <brokeskibusiness@gmail.com>
2025-07-20 13:16:03 +0300

    Implement cross-platform FileIO utility for file reading and writing

--- io/file_io.cpp
+++ io/file_io.cpp
@@ -1,5 +1,20 @@
-//
-// Created by zackn on 7/20/2025.
-//
-
 #include "file_io.hpp"
+#include <fstream>
+#include <sstream>
+
+std::string FileIO::readFile(const std::string& filepath) {
+    std::ifstream file(filepath);
+    if (!file.is_open()) return "";
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+bool FileIO::writeFile(const std::string& filepath, const std::string& data) {
+    std::ofstream file(filepath, std::ios::trunc);
+    if (!file.is_open()) return false;
+
+    file << data;
+    return true;
+}
--- io/file_io.hpp
+++ io/file_io.hpp
@@ -1,16 +1,19 @@
-//
-// Created by zackn on 7/20/2025.
-//
+#pragma once
+#include <string>
+#include <vector>
 
-#ifndef FILE_IO_HPP
-#define FILE_IO_HPP
+/**
+ * Simple cross-platform file reading and writing utility.
+ */
 
+class FileIO {
+public:
+    // Reads entire file into a string
+    static std::string readFile(const std::string& filepath);
 
+    // Writes string data to a file (overwrites)
+    static bool writeFile(const std::string& filepath, const std::string& data);
 
-class file_io {
-
+    // Reads file line by line into a vector of strings
+    static std::vector<std::string> readLines(const std::string& filepath);
 };
-
-
-
-#endif //FILE_IO_HPP
