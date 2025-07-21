85cc3cde6b4bc143988372cefdf461681cfd5020
ZackLackinJava <brokeskibusiness@gmail.com>
2025-07-21 15:11:55 +0300

    Remove unnecessary variable in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ int main() {
     std::cout << "Starting ToolLinux..." << std::endl;
 
     ConfigManager cfg;
-    bool loaded = cfg.load("config/default.cfg");
-
-    if (loaded) {
+    // no longer needed since we can just put that in 
+    // the if-statement instead of a separate variable
+    // bool loaded = cfg.load("config/default.cfg");
+    
+    if (cfg.load("config/default.cfg")) {
         std::cout << "Config loaded successfully." << std::endl;
     } else {
         std::cout << "Failed to load config file." << std::endl;
